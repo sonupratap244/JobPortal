0030d04444d970035af85f4c0937368c3e2f44b8
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2025-11-23 23:19:44 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true,autoIncrement: true  },
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: true },
